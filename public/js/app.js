35eed9bb499f178ab8c517038b7d4789adca74c3
Sean Reigle <sereigle@gmail.com>
2017-04-24 16:50:34 -0700

    added functional handlebars for projects

--- public/js/app.js
+++ public/js/app.js
@@ -9,3 +9,33 @@
 			$("#nav").removeAttr("style");
 		}
 	});
+
+	var articles = [];
+
+	function Article (obj) {
+	  this.title = obj.title;
+	  this.link = obj.link;
+	  this.image = obj.image;
+	  this.deployedOn = obj.deployedOn;
+	  this.body = obj.body;
+	}
+
+	Article.prototype.toHtml = function() {
+	  var template = $('#article-template').html();
+	  var render = Handlebars.compile(template);
+	  this.daysAgo = parseInt((new Date() - new Date(this.deployedOn))/60/60/24/1000);
+	  this.publishStatus = this.deployedOn ? `published ${this.daysAgo} days ago` : '(draft)';
+	  return render(this);
+	};
+
+	projectData.sort(function(a,b) {
+	  return (new Date(b.deployedOn)) - (new Date(a.deployedOn));
+	});
+
+	projectData.forEach(function(articleObject) {
+	  articles.push(new Article(articleObject));
+	});
+
+	articles.forEach(function(article) {
+	  $('#articles').append(article.toHtml());
+	});
